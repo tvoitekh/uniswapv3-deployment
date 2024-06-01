af1e1590b4eb253c65a20cc5ecb1c21016a12591
tvoitekh <timothy.voitekh@gmail.com>
2024-06-01 23:17:46 +0800

    deployed tokens and liquidity pool, added and verified liquidity

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,14 +4,15 @@ require('hardhat-gas-reporter');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const ZIRCUIT_PRIVATE_KEY = "386e891edb5c40917ff3eb0d8ca2bf6dc94849c4cfa94dadccf2ca31221fc259";
+const ZIRCUIT_PRIVATE_KEY1 = "386e891edb5c40917ff3eb0d8ca2bf6dc94849c4cfa94dadccf2ca31221fc259";
+const ZIRCUIT_PRIVATE_KEY2 = "ef9a1d961c5739294e411b8580da2ec820eca93421b4279a84602e5b0a47f130";
 
 module.exports = {
   solidity: "0.8.24",
   networks: {
     zircuit: {
       url: `https://zircuit1.p2pify.com`,
-      accounts: [ZIRCUIT_PRIVATE_KEY]
+      accounts: [ZIRCUIT_PRIVATE_KEY1 , ZIRCUIT_PRIVATE_KEY2]
     }
   },
   gasReporter: {
